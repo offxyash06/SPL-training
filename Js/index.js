428eb6057685a513e1fb49b898bc2fcfe074f1d1
offxyash06 <yashrunthp@gmail.com>
2026-03-11 12:02:00 +0530

    firstcommit

---
+++ Js/index.js
@@ -0,0 +1,53 @@
+ //console.log("Hello YAshrunth ");
+
+ //varName = 12.4;
+ //varName = 13;
+ //console.log(Name);
+
+ //let number =99;
+ //Number = 100;
+ //console.log(Number);
+
+ //const Num =13;
+ //Num =14;
+
+ //Bom
+ //alert("Warning....");
+ //prompt("Enter Your Name");
+ //confirm("R u exit this page ");
+ //open("https://www.youtube.com/"); 
+ //close()
+
+ //let Name =" Uttaranchal ";
+ //console.log(Name);
+ //console.log(Name.length);
+ //console.log(Name.concat(Name));
+ //console.log(Name.indexOf("a"));
+ //console.log(Name,lastIndexOf("a"));
+ //console.log(Name.repeat(2));
+ //console.log(Name.slice(0,2));
+ //console.log(Name.trim());          
+
+
+
+
+ function uttaranchal(){
+    console.log("Hello Welcome" );
+ }
+
+ uttaranchal("UIT"); 
+
+ function Home(){    return 12+12;}
+    console.log(Home());
+
+const Name = ()=>{ console.log("CSE14")}
+Name()
+
+setTimeout(()=>{
+    console.log("India");
+    clearInterval(y)
+},4000);
+
+
+let y=setInterval(()=>{console.log("DOON");
+    },1000)
